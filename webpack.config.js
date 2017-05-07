be99d43fbb9ee5e15259d8877ae1dcf7d653b44d
Michael Cittar <mcittar@gmail.com>
2017-05-07 10:44:33 -0700

    Set up webpack

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  entry: './lib/app.js',
+  output: {
+    filename: './lib/bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  },
+  devtool: 'source-maps'
+};
